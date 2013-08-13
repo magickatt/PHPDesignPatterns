20847b09e7467b3f98180feb691c16616b7479de
Andrew Kirkpatrick <development@andrew-kirkpatrick.com>
2013-08-13 21:44:32 +0100

    Finished Memento class

--- src/Memento/Memento.php
+++ src/Memento/Memento.php
@@ -2,32 +2,46 @@
 
 namespace Memento;
 
+/**
+ * Memento design pattern
+ * 
+ * There are various ways of tracking the changes made to an object, many that
+ * are more graceful and performant than this, but this should demonstrate a 
+ * relatively simple way to track the changes made to an object in a 
+ * transactional way
+ * @author Andrew Kirkpatrick <info@andrew-kirkpatrick.com>
+ */
 final class Memento
 {
     /**
-     * 
-     * @var unknown_type
+     * Name (of the thing)
+     * @var string
      */
     private $name;
     
     /**
-     * 
-     * @var unknown_type
+     * Gender (or sex if you're from a bygone era)
+     * @var string
      */
     private $gender;
     
     /**
-     * 
-     * @var unknown_type
+     * Age (in human years, not dog years)
+     * @var integer
      */
     private $age;
     
     /**
-     * 
-     * @var array Memento objects in LIFO stack
+     * Revision history of current Memento object
+     * @var Memento[] Memento objects in LIFO stack
      */
     private $revisions = array();
     
+    /**
+     * Get a property
+     * @param string $name
+     * @return mixed|null
+     */
     public function __get($name)
     {
         //
@@ -37,56 +51,73 @@ final class Memento
         return null;
     }
     
+    /**
+     * Set a property
+     * @param string $name
+     * @param mixed $value
+     */
     public function __set($name, $value)
     {
-        //
+        // If the property exists...
         if (property_exists($this, $name)) {
 
-            //
+            // Clone the current object and add as a revision
             $clone = clone($this);
             $this->addRevision($clone);
             
-            //
-            $clone->$name = $value;
+            // Set the property
+            $this->$name = $value;
         }
     }
     
+    /**
+     * Commit changes (discard all previous revisions)
+     * 
+     * Discards the previous revisions of this object so that changes are now 
+     * final
+     */
     public function commit()
     {
-        //
-        $latest = array_pop($this->revisions);
-        
-        //
-        foreach (get_object_vars($latest) as $property => $value) {
-            
-            //
-            $this->$property = $value;
-        }
-        
-        $this->revisions = array();
+        $this->clearRevisions();
     }
     
+    /**
+     * Rollback changes
+     * @param integer $depth How many revisions to rollback
+     */
     public function rollback($depth = 0)
     {
-        //
-        if ((integer) $depth > 0) {
+        // If no depth is specified, rollback to the oldest revision
+        if ((integer) $depth <= 0) {
+            $depth = count($this);
+        }
             
-            //
-            for ($x = 1; $x <= $depth; $x++) {
-                array_pop($this->revisions);
-            }
-
-        //
-        } else {
-            $this->clearRevisions();
+        // Pop revisions from the stack until the specified depth is reached
+        for ($x = 1; $x <= $depth; $x++) {
+            $rollbackRevision = array_pop($this->revisions);
+        }
+        
+        // Iterate over each property of the revision
+        foreach (get_object_vars($rollbackRevision) as $property => $value) {
+        
+            // Set the property of the current object to that of the revision
+            $this->$property = $value;
+            
         }
     }
     
+    /**
+     * Add a revision
+     * @param self $object
+     */
     private function addRevision(self $object)
     {
         $this->revisions[] = $object;
     }
     
+    /**
+     * Clear revisions
+     */
     private function clearRevisions()
     {
         $this->revisions = array();
